f463dd493957fbf1516b73a7bc5a86c741b830ab
VootLejin <alex.king.60137@gmail.com>
2017-06-17 15:30:48 -0400

    Made Cape Model

--- models/capemodel.js
+++ models/capemodel.js
@@ -1,3 +1,25 @@
 /**
  * Created by voot on 6/17/17.
  */
+
+var Mongoose = require('mongoose');
+var myConnection = Mongoose.connect('mongodb://localhost/capenet');
+
+var capeSchema = new Mongoose.Schema({
+    name: {
+        type: String,
+        default: 'Jyhn Doe'
+    },
+    powerTheme: String,
+    creationMethod: String,
+    powers: [{classification: String, rating: Number}],
+    description: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Cape = myConnection.model('Capecharacters', capeSchema);
+
+module.exports = Cape;
