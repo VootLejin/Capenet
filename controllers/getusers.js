0a24e63d1952f453877eab59ae6b82dc579944bc
VootLejin <alex.king.60137@gmail.com>
2017-06-17 14:46:36 -0400

    Able to read from Database and make dynamic page.

---
+++ controllers/getusers.js
@@ -0,0 +1,19 @@
+/**
+ * Created by voot on 6/17/17.
+ */
+var User = require('../models/user');
+
+var getusers = function(result, callback){
+    console.log("Getting Users...");
+    User.find(function(err, usersArray) {
+        if (err){
+            callback(err, null);
+        } else {
+            callback(null, usersArray);
+        }
+    });
+
+    console.log("Got Users...");
+};
+
+module.exports = getusers;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var makeuser = require('../controllers/makeuser');
+var getusers = require('../controllers/getusers');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  makeuser();
-  res.send('respond with a resource');
+  var userList = { users: [] };
+  getusers(userList, function(err, usersArray){
+    if (err){
+      //Error Response
+    }
+      usersArray.forEach(function (userName) {
+          userList.users.push(userName.name);
+      });
+      res.render('users', userList);
+  });
+
 });
 
 router.post('/', function(req,res, next){
