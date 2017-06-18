f26d7aed2db962cff70fd866ea2c54f09c3f12a6
VootLejin <alex.king.60137@gmail.com>
2017-06-18 19:21:19 -0400

    Basic Power Selector outline is operational.

--- public/javascripts/capturecapedata.js
+++ public/javascripts/capturecapedata.js
@@ -6,11 +6,16 @@ var app = angular.module('capeData',[]);
 
 app.controller('capeDataController', function($scope, $http){
     //$scope.name = 'John Doe';
+    $scope.defaultClasses = ['Mover', 'Shaker', 'Brute', 'Breaker',
+        'Master', 'Tinker', 'Blaster', 'Thinker',
+        'Striker', 'Changer', 'Trump', 'Stranger'];
+    $scope.powers = [{classification: 'Mover', rating: 0}];
 
     $scope.lazyInit = function(){
         $scope.name = "Fat Man";
         $scope.description = "Able to Eat an Entire sandwich in one bite, no matter the size. No, really, you should of seen it!";
-    }
+    };
+
     $scope.submit = function(){
         var capeInfo = {cape:{name: $scope.name}};
         $http.post("/cape", capeInfo)
@@ -18,5 +23,9 @@ app.controller('capeDataController', function($scope, $http){
                 $scope.creator = response.data.creator;
             });
     };
+
+    $scope.addPowerClass = function(){
+
+    }
 
 });
