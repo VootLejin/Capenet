5a1c9477773a9190bc377d5d7150ed9fa95b8f79
VootLejin <alex.king.60137@gmail.com>
2017-06-18 17:53:10 -0400

    Making the cape edit page look better.

--- public/javascripts/capturecapedata.js
+++ public/javascripts/capturecapedata.js
@@ -5,8 +5,12 @@
 var app = angular.module('capeData',[]);
 
 app.controller('capeDataController', function($scope, $http){
-    $scope.name = 'John Doe';
+    //$scope.name = 'John Doe';
 
+    $scope.lazyInit = function(){
+        $scope.name = "Fat Man";
+        $scope.description = "Able to Eat an Entire sandwich in one bite, no matter the size. No, really, you should of seen it!";
+    }
     $scope.submit = function(){
         var capeInfo = {cape:{name: $scope.name}};
         $http.post("/cape", capeInfo)
