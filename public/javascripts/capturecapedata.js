58adfed09a1241cebbf34c9e9afab9c97b5aa855
VootLejin <alex.king.60137@gmail.com>
2017-06-19 14:02:27 -0400

    Power Creation is online. Needs more Generational results, and there is no check for points atm.

--- public/javascripts/capturecapedata.js
+++ public/javascripts/capturecapedata.js
@@ -10,6 +10,8 @@ app.controller('capeDataController', function($scope, $http){
         'Master', 'Tinker', 'Blaster', 'Thinker',
         'Striker', 'Changer', 'Trump', 'Stranger'];
     $scope.powers = [{classification: 'Mover', rating: 0}];
+    $scope.basePoints = 0;
+    $scope.points = 0;
 
     $scope.lazyInit = function(){
         $scope.name = "Fat Man";
@@ -17,7 +19,11 @@ app.controller('capeDataController', function($scope, $http){
     };
 
     $scope.submit = function(){
-        var capeInfo = {cape:{name: $scope.name}};
+        var capeInfo = {cape:{
+            name: $scope.name,
+            description: $scope.description,
+            powers: $scope.powers,
+            powerTheme: $scope.powerTheme}};
         $http.post("/cape", capeInfo)
             .then(function success(response){
                 $scope.creator = response.data.creator;
@@ -25,7 +31,31 @@ app.controller('capeDataController', function($scope, $http){
     };
 
     $scope.addPowerClass = function(){
+        $scope.powers.push({classification: 'Mover', rating: 0});
+    };
 
-    }
+    $scope.randomPower = function(){
+        // roll d20, get value
+        $scope.points = Math.floor(Math.random()*20)+1;
+        // get # of classes
+        var numClasses = Math.floor(Math.random() * 3) + 1;
+        if ($scope.points > 10) {
+            numClasses +=1;
+        }
+        $scope.numClasses = numClasses;
+        // get random classes
+        $scope.powers=[];
+        var classID =0;
+        for(var i = 0; i < numClasses; i++){
+            //console.log("Adding power Class");
+            $scope.addPowerClass();
+            //console.log("Picking Class...");
+            classID = Math.floor(Math.random() * $scope.defaultClasses.length);
+            //console.log("Adding Class: " + classID);
+            //console.log($scope.defaultClasses[classID]);
+            $scope.powers[i] = {classification: $scope.defaultClasses[classID], rating: 0};
+        }
+
+    };
 
 });
