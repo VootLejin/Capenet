c639d9d813d44f3ab4c59ff0e1c87f48c8a6a403
VootLejin <alex.king.60137@gmail.com>
2017-06-30 17:11:02 -0400

    Prevents posting of more then one cape per minute.

--- app/controllers/cape.js
+++ app/controllers/cape.js
@@ -6,66 +6,93 @@ var Cape = require('../models/capemodel');
 // Because we have to update the user on edits and creation
 var User = require('../models/usermodel');
 
+var canSubmit = function(result, callback){
+    var now = Date.now();
+    var elapsed = now - result.lastSubmissionDate;
+    if (elapsed < 60000 ){ // 1 minute in milliseconds
+        callback(false);
+    } else {
+        callback(true);
+    }
+};
+var checkCapeSubmissionDelay = function(user, capeInfo, callback){
+  User.findOne({username: user}, 'lastSubmissionDate', function(err, result){
+    canSubmit(result, function(abletopost){
+        if(abletopost){
+            makeCape(capeInfo, user, callback);
+        } else {
+            callback(null, {failed: true, _reason:'You must wait one minute between cape submissions'});
+        }
+    })
+  });
+};
+
+var makeCape = function(capeInfo, user, callback){
+    var myCape = new Cape({});
+
+    //set dateCreated
+    myCape.dateCreated = Date.now();
+
+    //set creator
+    myCape.creator = user;
+
+
+    //set name
+    if(capeInfo.name){
+        if(capeInfo.name.length > 32){
+            myCape.name = 'unnamed'
+        } else {
+            myCape.name = capeInfo.name;
+        }
+
+    } else {
+        // To Do, some sort of auto increment for unnamed cases
+        myCape.name = 'unnamed';
+    }
+
+    //set powerTheme
+    if(capeInfo.powerTheme){
+        myCape.powerTheme = capeInfo.powerTheme;
+    }
+
+    //set creation method
+    if(capeInfo.creationMethod){
+        myCape.creationMethod = capeInfo.creationMethod;
+    }
+
+    //set description
+    if (capeInfo.description){
+        myCape.description = capeInfo.description;
+    }
+
+    //set powers
+    if (capeInfo.powers){
+        myCape.powers = capeInfo.powers;
+    } else {
+        myCape.powers[0] = {'classification' : 'Thinker', 'rating' : 0};
+    }
+
+    // Save Cape
+    myCape.save(function(err, savedCape, numEffected){
+        if (err)
+            callback(err);
+        // Now update user
+        User.updateLastSubmission(user, savedCape._id, function(err){
+            if (err){
+                callback(err);
+            }
+        })
+    });
+
+    callback(null, myCape);
+};
 var CapeController = {
     /* Create */
     makeCape    : function(capeInfo,user, callback){
-        var myCape = new Cape({});
-
-        //set dateCreated
-        myCape.dateCreated = Date.now();
-
-        //set creator
-        myCape.creator = user;
+        // Check if user is able to make new cape
+        checkCapeSubmissionDelay(user, capeInfo, callback);
 
 
-        //set name
-        if(capeInfo.name){
-            if(capeInfo.name.length > 32){
-                myCape.name = 'unnamed'
-            } else {
-                myCape.name = capeInfo.name;
-            }
-
-        } else {
-            // To Do, some sort of auto increment for unnamed cases
-            myCape.name = 'unnamed';
-        }
-
-        //set powerTheme
-        if(capeInfo.powerTheme){
-            myCape.powerTheme = capeInfo.powerTheme;
-        }
-
-        //set creation method
-        if(capeInfo.creationMethod){
-            myCape.creationMethod = capeInfo.creationMethod;
-        }
-
-        //set description
-        if (capeInfo.description){
-            myCape.description = capeInfo.description;
-        }
-
-        //set powers
-        if (capeInfo.powers){
-            myCape.powers = capeInfo.powers;
-        } else {
-            myCape.powers[0] = {'classification' : 'Thinker', 'rating' : 0};
-        }
-        myCape.save(function(err, savedCape, numEffected){
-            console.log('cape creation save callback');
-            if (err)
-                callback(err);
-            // Now update user
-            User.updateLastSubmission(user, savedCape._id, function(err){
-                console.log('cape creation userUpdate callback');
-                if (err){
-                    callback(err);
-                }
-            })
-        });
-
-        callback(null, myCape);
     },
 
     /* Read */
