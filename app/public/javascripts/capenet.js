b5aed00047851b3b2ec227fe7c5f7664e8fc9758
VootLejin <alex.king.60137@gmail.com>
2017-07-05 12:58:19 -0400

    Cleaned up some stray code in preload.

--- app/public/javascripts/capenet.js
+++ app/public/javascripts/capenet.js
@@ -2,7 +2,7 @@
  * Created by voot on 6/21/17.
  */
 
-var app = angular.module('capenet',['ngRoute', 'ngResource']);
+var app = angular.module('capenet',['ngRoute', 'ngResource', 'infinite-scroll']);
 
 app.factory('capeHandlingFactory', function userModel($rootScope){
     return{
--- app/public/javascripts/preload.js
+++ app/public/javascripts/preload.js
@@ -1,8 +1,6 @@
 /**
  * Created by voot on 6/19/17.
  */
-
-var app = angular.module('capenet', ['ngRoute', 'ngResource']);
 Array.prototype.resize =  function(newSize){
     while (newSize > this.length){
         this.push(0);
